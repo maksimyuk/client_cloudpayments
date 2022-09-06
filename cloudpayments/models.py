fd7e3935592779750e73fe251ba8ad0d261c3a25
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-09-06 15:41:55 +0300

    * added marshmallow-enum requirement; * added transaction schema; * added failing test for schema.

--- cloudpayments/models.py
+++ cloudpayments/models.py
@@ -1,5 +1,8 @@
 
 from marshmallow import INCLUDE, Schema, fields
+from marshmallow_enum import EnumField
+
+from cloudpayments.enum import Currency
 
 
 class ChargeErrorModelSchema(Schema):
@@ -38,3 +41,69 @@ class Secure3dAuthenticationSchema(Schema):
     frame_height = fields.Integer(data_key='FrameHeight', allow_none=True)
     three_ds_session_callback_id = fields.String(data_key='ThreeDsCallbackId')
     escrow_accumulation_id = fields.String(data_key='EscrowAccumulationId', allow_none=True)
+
+
+class TransactionSchema(Schema):
+    """Model for successful Transaction response"""
+
+    reason_code = fields.Integer(data_key='ReasonCode', required=True)
+    public_id = fields.String(data_key='PublicId')
+    terminal_url = fields.URL(data_key='TerminalUrl')
+    transaction_id = fields.Integer(data_key='TransactionId', required=True)
+    amount = fields.Float(data_key='Amount', required=True)
+    currency = EnumField(Currency, data_key='Currency')
+    currency_code = fields.Integer(data_key='CurrencyCode')
+    payment_amount = fields.Integer(data_key='PaymentAmount')
+    payment_currency = EnumField(Currency, data_key='PaymentCurrency')
+    payment_currency_code = fields.Integer(data_key='PaymentCurrencyCode')
+    invoice_id = fields.String(data_key='InvoiceId')
+    account_id = fields.String(data_key='AccountId')
+    email = fields.Email(data_key='Email', allow_none=True)
+    description = fields.String(data_key='Description')
+    json_data = fields.Dict(data_key='JsonData', allow_none=True)
+    created_date = fields.DateTime(data_key='CreatedDate', format='timestamp')
+    payout_date = fields.DateTime(data_key='PayoutDate', allow_none=True)
+    payout_date_iso = fields.DateTime(data_key='PayoutDateIso', allow_none=True)
+    payout_amount = fields.Float(data_key='PayoutAmount', allow_none=True)
+    created_date_iso = fields.DateTime(data_key='CreatedDateIso')
+    auth_date = fields.DateTime(data_key='AuthDate', format='timestamp')
+    auth_date_iso = fields.DateTime(data_key='AuthDateIso')
+    confirm_date = fields.DateTime(data_key='ConfirmDate', format='timestamp', allow_none=True)
+    confirm_date_iso = fields.DateTime(data_key='ConfirmDateIso', allow_none=True)
+    auth_code = fields.String(data_key='AuthCode')
+    test_mode = fields.Bool(data_key='TestMode')
+    rrn = fields.String(data_key='Rrn', allow_none=True)
+    original_transaction_id = fields.Integer(data_key='OriginalTransactionId', allow_none=True)
+    fall_back_scenario_declined_transaction_id = fields.Integer(data_key='FallBackScenarioDeclinedTransactionId', allow_none=True)
+    ip_address = fields.IPv4(data_key='IpAddress')
+    ip_country = fields.String(data_key='IpCountry')
+    ip_city = fields.String(data_key='IpCity')
+    ip_region = fields.String(data_key='IpRegion')
+    ip_district = fields.String(data_key='IpDistrict')
+    ip_latitude = fields.Number(data_key='IpLatitude')
+    ip_longitude = fields.Number(data_key='IpLongitude')
+    card_first_six = fields.String(data_key='CardFirstSix')
+    card_last_four = fields.String(data_key='CardLastFour')
+    card_expiration_date = fields.String(data_key='CardExpDate')
+    card_type = fields.String(data_key='CardType')
+    card_product = fields.String(data_key='CardProduct')
+    card_category = fields.String(data_key='CardCategory')
+    escrow_accumulation_id = fields.Integer(data_key='EscrowAccumulationId', allow_none=True)
+    issuer_bank_country = fields.String(data_key='IssuerBankCountry')
+    issuer = fields.String(data_key='Issuer')
+    card_type_code = fields.Integer(data_key='CardTypeCode')
+    status = fields.String(data_key='Status')
+    status_code = fields.Integer(data_key='StatusCode')
+    culture_name = fields.String(data_key='CultureName')
+    reason = fields.String(data_key='Reason')
+    card_holder_message = fields.String(data_key='CardHolderMessage')
+    type_ = fields.Integer(data_key='Type')
+    refunded = fields.Bool(data_key='Refunded')
+    name = fields.String(data_key='Name')
+    token = fields.String(data_key='Token')
+    subscription_id = fields.Integer(data_key='SubscriptionId', allow_none=True)
+    gateway_name = fields.String(data_key='GatewayName')
+    apple_pay = fields.Bool(data_key='ApplePay')
+    android_pay = fields.Bool(data_key='AndroidPay')
+    wallet_type = fields.String(data_key='WalletType')
+    total_fee = fields.Float(data_key='TotalFee')
--- tests/test_models/test_models.py
+++ tests/test_models/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cloudpayments.models import Secure3dAuthenticationSchema
+from cloudpayments.models import Secure3dAuthenticationSchema, TransactionSchema
 
 
 @pytest.fixture()
@@ -26,5 +26,83 @@ class TestSecure3dAuthenticationSchema:
     def test_serialization(self, raw_secure_3d_response):
         """Check for successful serialization."""
         serialized = Secure3dAuthenticationSchema().load(raw_secure_3d_response)
+
+        assert serialized
+
+
+@pytest.fixture()
+def raw_transaction_response() -> dict:
+    """Fixture for response with transaction data."""
+    return {
+        "ReasonCode": 0,
+        "PublicId": "pk_********************************",
+        "TerminalUrl": "http://test.test",
+        "TransactionId": 891510444,
+        "Amount": 10,
+        "Currency": "RUB",
+        "CurrencyCode": 0,
+        "PaymentAmount": 10,
+        "PaymentCurrency": "RUB",
+        "PaymentCurrencyCode": 0,
+        "InvoiceId": "1234567",
+        "AccountId": "user_x",
+        "Email": None,
+        "Description": "Оплата товаров в example.com",
+        "JsonData": None,
+        "CreatedDate": "/Date(1635150224630)/",
+        "PayoutDate": None,
+        "PayoutDateIso": None,
+        "PayoutAmount": None,
+        "CreatedDateIso": "2021-10-25T08:23:44",
+        "AuthDate": "/Date(1635150224739)/",
+        "AuthDateIso": "2021-10-25T08:23:44",
+        "ConfirmDate": None,
+        "ConfirmDateIso": None,
+        "AuthCode": "A1B2C3",
+        "TestMode": True,
+        "Rrn": None,
+        "OriginalTransactionId": None,
+        "FallBackScenarioDeclinedTransactionId": None,
+        "IpAddress": "123.123.123.123",
+        "IpCountry": "CN",
+        "IpCity": "Beijing",
+        "IpRegion": "Beijing",
+        "IpDistrict": "Beijing",
+        "IpLatitude": 39.9289,
+        "IpLongitude": 116.3883,
+        "CardFirstSix": "411111",
+        "CardLastFour": "1111",
+        "CardExpDate": "11/25",
+        "CardType": "Visa",
+        "CardProduct": "C",
+        "CardCategory": "Visa Signature (Signature)",
+        "EscrowAccumulationId": None,
+        "IssuerBankCountry": "RU",
+        "Issuer": "CloudPayments",
+        "CardTypeCode": 0,
+        "Status": "Authorized",
+        "StatusCode": 2,
+        "CultureName": "ru",
+        "Reason": "Approved",
+        "CardHolderMessage": "Оплата успешно проведена",
+        "Type": 0,
+        "Refunded": False,
+        "Name": "CARDHOLDER NAME",
+        "Token": "0a0afb77-8f41-4de2-9524-1057f9695303",
+        "SubscriptionId": None,
+        "GatewayName": "Test",
+        "ApplePay": False,
+        "AndroidPay": False,
+        "WalletType": "",
+        "TotalFee": 0
+    }
+
+
+class TestTranscactionSchema:
+    """Test case for checking serialization."""
+
+    def test_serialization(self, raw_transaction_response):
+        """Check for successful serialization."""
+        serialized = TransactionSchema().load(raw_transaction_response)
 
         assert serialized
