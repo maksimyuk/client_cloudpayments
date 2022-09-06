a749e017fed3f09f83890aab5632d4674fd0e250
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-09-06 16:27:05 +0300

    * name fixes;

--- cloudpayments/processors.py
+++ cloudpayments/processors.py
@@ -21,7 +21,7 @@ class ChargeProcessor(BaseMethodProcessor):
 
         # Making request
         try:
-            raw_response = await client.post(
+            response = await client.post(
                 interaction_method=interaction_method,
                 url=endpoint_url,
                 **ChargeRequestSchema().dump(schema),
@@ -30,13 +30,13 @@ class ChargeProcessor(BaseMethodProcessor):
             raise
 
         # Parsing result
-        if raw_response['success']:
-            return TransactionSchema().load(raw_response['Model'])
+        if response['success']:
+            return TransactionSchema().load(response['Model'])
 
-        if raw_response['message']:
+        if response['message']:
             return 'Message'
 
-        if raw_response['model']:
+        if response['model']:
             return 'Model'
 
-        return Secure3dAuthenticationSchema().load(raw_response['Model'])
+        return Secure3dAuthenticationSchema().load(response['Model'])
