9059ab627544833d35395794507504cdbb61524f
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-09-06 11:18:50 +0300

    * docstring

--- cloudpayments/methods.py
+++ cloudpayments/methods.py
@@ -3,7 +3,12 @@ from cloudpayments.processors import ChargeProcessor
 
 
 class CloudPayment:
+    """
+    Class for describing client methods to interact with cloudpayments.
+    Add new method with processor to interact by cloudpayments.
+    """
 
     @classmethod
     def charge(cls, *args, **kwargs):
+        """Call charge API method of cloudpayments."""
         return ChargeProcessor(*args, **kwargs).process()
