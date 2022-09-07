1f6e40c189701916f41cf62753152a4ad15788ad
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-09-07 15:04:02 +0300

    * mior fix

--- cloudpayments/config.py
+++ cloudpayments/config.py
@@ -2,5 +2,5 @@
 import os
 
 
-LOGIN = os.environ['CLOUD_PAYMENT_API_LOGIN'] or 'test'
-PASSWORD = os.environ['CLOUD_PAYMENT_API_PASSWORD'] or 'test'
+LOGIN = os.getenv('CLOUD_PAYMENT_API_LOGIN') or 'test'
+PASSWORD = os.getenv('CLOUD_PAYMENT_API_PASSWORD') or 'test'
